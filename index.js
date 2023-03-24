117d1238881a8c982b4fd3be87503dbc796b5b39
Arzu Caner <arzuguneycaner@gmail.com>
2023-03-24 09:54:34 +0000

    Update color-lighten-darken-tool

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const hexInput =document.getElementById('hexInput');
-const InputColor = document.getElementById('inputColor');
+const inputColor = document.getElementById('inputColor');
 const alteredColer = document.getElementById('alteredColor');
 const alteredColorText = document.getElementById('alteredColortext');
 const slider = document.getElementById('slider');
@@ -28,7 +28,7 @@ hexInput.addEventListener('keyup', () => {
 
     const strippedHex = hex.replace('#', '');
 
-    InputColor.style.backgroundColor = "#" + strippedHex;
+    inputColor.style.backgroundColor = "#" + strippedHex;
     reset();
 })
 
@@ -54,10 +54,10 @@ const convertHexToRGB = (hex) => {
     const g = parseInt(strippedHex.substring(2,4), 16);
     const b = parseInt(strippedHex.substring(4,6), 16);
 
-    return {r,g,b}
+    return {r,g,b};
 }
 
- const converRGBToHex = (r,g,b) => {
+ const convertRGBToHex = (r,g,b) => {
     const firstPair = ("0" + r.toString(16)).slice(-2);
     const secondPair = ("0" + g.toString(16)).slice(-2);
     const thirdPair = ("0" + b.toString(16)).slice(-2);
@@ -67,14 +67,14 @@ const convertHexToRGB = (hex) => {
  }
 
  const alterColor = (hex, percentage) => {
-    const {r, g, b} =convertHexToRGB(hex);
+    const {r, g, b} = convertHexToRGB(hex);
 
     const amount = Math.floor((percentage/100) * 255);
 
     const newR = increaseWithin0To255(r,amount);
     const newG = increaseWithin0To255(g,amount);
-    const newB = increaseWithin0To255(b,amount);
-    return converRGBToHex(newR, newG, newB);
+    const newB = increaseWithin0To255(b,amount)
+    return convertRGBToHex(newR, newG, newB);
   }
 
   const increaseWithin0To255 = (hex, amount) => {
